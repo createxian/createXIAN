030c79a833e1b9edd690b4afab3c17460cfab710
CallMeSaltyF1sh <643342586@qq.com>
2019-07-14 13:04:56 +0800

    policy&&details update

--- Creat_Xi'an/pages/policyDetails/policyDetails.js
+++ Creat_Xi'an/pages/policyDetails/policyDetails.js
@@ -5,14 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    fileUrl: '',
+    level: '',
+    institution: '',
+    originalUrl: '',
+    title: '',
+    content: '',
+    id: '',
+    time: '',
+    catg: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let self = this;
+    let id = options.id;
+    wx.request({
+      url: 'https://cxa.wizzstudio.com/policy/detail/'+id,
+      method: 'GET',
+      success: res => {
+        console.log(res);
+        let data = res.data.data;
+        self.setData({
+          fileUrl: data.fileUrl,
+          originalUrl: data.originalUrl,
+          institution: data.publishInstitution,
+          catg: data.classify,
+          title: data.poicyTitle,
+          content: data.policyContent,
+          id: data.policyId,
+          time: data.publishTime,
+          level: data.level
+        })
+      }
+    })
   },
 
   /**
